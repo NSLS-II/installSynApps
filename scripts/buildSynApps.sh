b48ec91f3cd2d6a89f503ccbc8f772ba96e3b621
jwlodek <jwlodek@cs.stonybrook.edu>
2019-02-01 15:37:55 -0500

    added support for custom dependencies

--- scripts/buildSynApps.sh
+++ scripts/buildSynApps.sh
@@ -2,4 +2,11 @@
 
 # File that builds EPICS base and synApps from scratch with one call
 
+# First we clone all of the modules
+python3 clone_and_checkout.py
+python3 updare_release_file.py
+
+# first build the dependencies
 ./installDependencies.sh
+
+python3 buildEPICS.py
--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
 # Script that installs all
-# dependencies for modules being built
+# dependencies for modules being built.
+# 
+# For any custom dependencies, please add them here prior to running the buildSynApps.sh script
 #
 
+# Path variables, only used for adding custom dependencies (mostly for detector drivers)
+INSTALL=/home/jwlodek/Documents/testCloneRun
+EPICS_BASE=$INSTALL/base
+SUPPORT=$INSTALL/support
+AREA_DETECTOR=$SUPPORT/areaDetector
 
+# Example custom dependency, we must build libuvc before compiling ADUVC
+#ADUVC=$AREA_DETECTOR/ADUVC
+
+cd $INSTALL
+
+sudo apt install gcc
+sudo apt install g++
+sudo apt install make
 sudo apt install libxml2-dev
 sudo apt install libboost-dev
 sudo apt install libre2c-dev
 sudo apt install libopencv-dev
 sudo apt install libzbar-dev
-sudo apt install lib readline-dev
+sudo apt install libreadline-dev
+
+
+if [[ $ADUVC ]]
+then
+echo "Installing ADUVC dependencies"
+cd $ADUVC/adUVCSupport
+./installlibuvc.sh
+cd $INSTALL
+fi
