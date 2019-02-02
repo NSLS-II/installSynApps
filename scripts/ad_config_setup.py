305cb222dd7d0dbe6daf088938ad1151b559dd3f
jwlodek <jwlodek@cs.stonybrook.edu>
2019-02-02 17:03:28 -0500

    added python script modified from adConfigSetup script for autosetup of AD config files

---
+++ scripts/ad_config_setup.py
@@ -0,0 +1,115 @@
+# Modification of the full adConfigSetup script for use with the installSynApps package
+#
+# Author: Jakub Wlodek
+# Copyright (c): Brookhaven National Laboratory
+# Created: 2-Feb-2019
+
+import os
+import shutil
+import argparse
+
+
+# Global variables. isLinux is necessary because of the .Linux file being univeral across Linux arches
+isLinux = True
+EPICS_ARCH = "linux-x86_64"
+
+
+# Function that removes all whitespace in a string
+# Used prior to splitting a macro/value pair on the '=' symbol.
+def remove_whitespace(line):
+    line.strip()
+    no_whitespace = line.replace(" ", "")
+    return no_whitespace
+
+
+
+# Function that prints macro value pairs. Used to test external config file support.
+def print_pair_list(macro_value_pairs):
+    for pair in macro_value_pairs:
+        print("A value of '{}' will be assigned to the '{}' macro.\n".format(pair[1], pair[0]))
+
+
+
+# Function that iterates over the configuration files that pertain to the current arch,
+# identifies lines that contain the macros in the list, and replaces them. If not in the list 
+# the line is copied as-is. upon completion, the old file is moved into a new "EXAMPLE_FILES"
+# directory if it is needed again.
+#
+# @params: old_path                 -> path to the example configuration file
+# @params: required_pairs           -> pairs macros and values that are guaranteed to be replaced
+# @params: optional_pairs           -> pairs macros and values that can optionally be replaced
+# @params: replace_optional_macros  -> flag to see if optional macros will be replaced
+# @params: replace_commented        -> flag that decides if commented macros are to be replaced as well
+# @return: void
+#
+def copy_macro_replace(filename, path_to_configure, required_pairs, replace_commented = False):
+    old_file = open(path_to_configure + filename, "r+")
+    # Every example file starts with EXAMPLE_FILENAME, so we disregard the first 8 characters 'EXAMPLE_' for the new name
+    new_path = path_to_configure + filename[8:]
+    new_file = open(new_path, "w+")
+    # Iterate over the lines in the old file, replacing macros as you go
+    line = old_file.readline()
+    while line:
+        # check if the line contains a macro
+        was_macro = False
+        for pair in required_pairs:
+            # check if we want to replace the commented macro
+            if replace_commented and line[0] == '#':
+                line = line[1:]
+            if line.startswith(pair[0]):
+                new_file.write("{}={}\n".format(pair[0],pair[1]))
+                was_macro = True
+        # Otherwise just write line as-is
+        if was_macro == False:
+            new_file.write(line)
+        line = old_file.readline()
+    old_file.close()
+    # Place the old file in a directory for future use.
+    shutil.move(path_to_configure + filename, path_to_configure + "EXAMPLE_FILES/{}".format(filename))
+    new_file.close()
+
+
+
+# Removes unnecessary example files i.e. vxworks, windows etc.
+# This cleans up the configuration directory, and only leaves necessary files.
+#
+# If building for multilple architectures, do not use the -r flag enabling this.
+# In the future, a toggle between arches will be looked at.
+#
+# @return: void
+#
+def remove_examples(path_to_configure):
+    for file in os.listdir(path_to_configure):
+        if os.path.isfile(path_to_configure+file):
+            if file.startswith("EXAMPLE") and not file.endswith(".local") and not file.endswith(EPICS_ARCH):
+                if not file.endswith(".Linux"):
+                    os.remove(path_to_configure+file)
+                else:
+                    if not isLinux:
+                        os.remove(path_to_configure+file)
+
+
+
+# Basic function that iterates over all of the files and directories in the "configure" directory
+# of area detector. If it detects "EXAMPLE" files, it passes them on to the macro replacing function
+#
+# @params: required_pairs           -> recquired macro/value pairs
+# @params: optrional_pairs          -> optional macro/value pairs
+# @params: replace_optional_macros  -> flag to see if optional macro/value pairs should be replaced
+# @params: replace_commented        -> flag that decides if commented macros can also be replaced
+# @return: void
+#
+def process_examples(path_to_configure, required_pairs):
+    for file in os.listdir(path_to_configure):
+        if os.path.isfile(path_to_configure+file):
+            if file.startswith("EXAMPLE"):
+                copy_macro_replace(file, path_to_configure, required_pairs)
+
+
+def update_ad_releases(path_to_ad, required_pairs):
+    path_to_configure = path_to_ad +"/configure/"
+    os.mkdir(path_to_configure+"EXAMPLE_FILES")
+    #print(path_to_ad+", "+path_to_configure+"\n")
+    #print_pair_list(required_pairs)
+    remove_examples(path_to_configure)
+    process_examples(path_to_configure, required_pairs)
