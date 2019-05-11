2e8cf9fee7f5206719e77477737fcc38079ca43c
Jakub Wlodek <jwlodek@cs.stonybrook.edu>
2019-05-10 22:53:00 -0400

    Update auto_build.sh

--- auto_build.sh
+++ auto_build.sh
@@ -15,8 +15,23 @@ echo "Welcome to the installSynApps module."
 echo "It is designed to automate the build process for EPICS and areaDetector."
 echo "The scripts included will automatically edit all configuration files required, and then build with make."
 
-INSTALL_DIR=`grep INSTALL= ../configure/INSTALL_CONFIG`
+INSTALL_DIR=`grep INSTALL= configure/INSTALL_CONFIG`
 INSTALL=${INSTALL_DIR/INSTALL=/}
 
 echo "The current install directory is $INSTALL, is this ok?"
 echo "(y/n)"
+read response
+
+if [ "$response" = "y" ];
+then
+echo "Proceeding with Install location $INSTALL"
+else
+echo "Please enter a custom install location:"
+echo "INSTALL="
+read INSTALL
+fi
+
+echo "Beginning auto_build process"
+/scripts/installSynApps.sh
+
+echo "Completed auto_build process"
