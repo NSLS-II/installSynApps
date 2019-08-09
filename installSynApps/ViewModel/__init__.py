fc5dc598a8c9d16c43cb4762d9cf1f87428bfcf4
jwlodek <jwlodek@cs.stonybrook.edu>
2019-08-09 15:55:31 -0400

    Added new screen class for adding custom build scripts

--- installSynApps/ViewModel/__init__.py
+++ installSynApps/ViewModel/__init__.py
@@ -1,4 +1,5 @@
 import installSynApps.ViewModel.add_module_screen
+import installSynApps.ViewModel.add_custom_build_screen
 import installSynApps.ViewModel.edit_individual_module
 import installSynApps.ViewModel.edit_injector_screen
 import installSynApps.ViewModel.edit_install_screen
