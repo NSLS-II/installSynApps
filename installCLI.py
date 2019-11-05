897c81cb0c72b1a411898c09be19862db0f0d4a0
jwlodek <jwlodek@cs.stonybrook.edu>
2019-11-05 13:04:37 -0500

    Fixing python3 shebang to account for venv

--- installCLI.py
+++ installCLI.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 """ Python script for running the installSynApps module through the CLI """
 
--- installGUI.py
+++ installGUI.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 """ GUI class for the installSynApps module
 
