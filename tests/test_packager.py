cdb6e1f3ba59b383276ee794bc11093382e0f27f
jwlodek <jwlodek@cs.stonybrook.edu>
2019-11-12 17:27:29 -0500

    Updating unit tests to match new changes

--- tests/test_packager.py
+++ tests/test_packager.py
@@ -6,6 +6,8 @@ __author__      = "Jakub Wlodek"
 __copyright__   = "Copyright June 2019, Brookhaven Science Associates"
 
 import pytest
+import os
+import shutil
 import installSynApps.IO as IO
 import installSynApps.Driver as Driver
 
@@ -14,12 +16,9 @@ parser = IO.config_parser.ConfigParser('tests/TestConfigs/basic')
 install_config, message = parser.parse_install_config(allow_illegal=True)
 packager = Driver.packager_driver.Packager(install_config)
 
-def test_get_ad_modules():
-    out = packager.get_drivers_to_package()
-    assert len(out) == 1
-    assert out[0].name == 'DUMMY'
-
-def test_get_support_modules():
-    out = packager.get_modules_to_package()
-    assert len(out) == 1
-    assert out[0].name == 'MODBUS'
+def test_grab_folder():
+    os.mkdir('__TEMP__')
+    packager.grab_folder('../TestConfigs/basic', '__TEMP__')
+    for file in os.listdir('__TEMP__'):
+        assert os.path.exists('../TestConfigs/basic/' + file)
+    shutil.rmtree('__TEMP__')
