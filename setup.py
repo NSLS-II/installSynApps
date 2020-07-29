410412d2b9335e5c2e3c0bcb6fc027c5272ab8fc
jwlodek <jakub.wlodek@stonybrook.edu>
2020-07-29 17:29:54 -0400

    Update requirements and setup files

--- setup.py
+++ setup.py
@@ -10,26 +10,26 @@ with open('README.md') as readme:
 
 
 setuptools.setup(
-    name='installSynApps',
+    name='epics-install',
     description='A Python program for building EPICS and synApps',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.2.5',
+    version='0.2.7',
     author='Jakub Wlodek',
     author_email='jwlodek@bnl.gov',
     license='BSD (3-clause)',
     url='https://github.com/epicsNSLS2-deploy/installSynApps',
     packages=setuptools.find_packages(exclude=['tests', 'docs', '__pycache__']),
     py_modules=['installCLI', 'installGUI'],
-    package_data={'configure': ['*'], 'resources': ['*']},
+    #package_data={'configure': ['*'], 'resources': ['*']},
     include_package_data=True,
     python_requires='>=3.4',
     install_requires=requirements,
     keywords='epics install build deploy scripting automation',
     entry_points={
         'console_scripts': [
-            'installEPICS = installCLI:main',
-            'installEPICSGUI = installGUI:main',
+            'epics-install = installCLI:main',
+            'epics-install-gui = installGUI:main',
         ],
     },
     extras_require={
