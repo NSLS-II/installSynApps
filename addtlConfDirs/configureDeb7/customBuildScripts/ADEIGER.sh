f4d452e474b877273dc423a3372f58f486cc1a36
jwlodek <jwlodek.dev@gmail.com>
2020-09-23 15:54:32 -0400

    Modify deb 7 config to build ADEiger

--- addtlConfDirs/configureDeb7/customBuildScripts/ADEIGER.sh
+++ addtlConfDirs/configureDeb7/customBuildScripts/ADEIGER.sh
@@ -1,6 +1,17 @@
 
 
 wget https://github.com/zeromq/libzmq/releases/download/v4.3.3/zeromq-4.3.3.tar.gz
-tar -xzf zeromq-4.3.3.tar.gz
-mv zeromq-4.3.3.tar.gz zmq
+tar -xzf zeromq-4.3.3.tar.gz 
+mkdir zmq
+rm zeromq-4.3.3.tar.gz
+cd zeromq-4.3.3
+
+./autogen.sh
+./configure --prefix=$(pwd)/../zmq
+make
+make install
+cd ..
+rm -rf zeromq-4.3.3
+
 make -sj
+cp -r zmq/lib/* lib/linux-x86_64/.
